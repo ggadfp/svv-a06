f5ab65e0d55e0ccdeeaa4a0823ee2c88e4052d86
unknown <i.benyahia@hotmail.com>
2020-02-27 17:13:09 +0100

    E and G are updated!!!!

--- Numerical/Parameters.py
+++ Numerical/Parameters.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 
 
 # Material properties
-E = 72.9e9 # [Pa] -- E-modulus
-G = 27.1e9 # [Pa] -- Shear modulus
+E = 73.1e9 # [Pa] -- E-modulus
+G = 28e9 # [Pa] -- Shear modulus
 
 # Number of nodes
 Nx = 41 # Spanwise Nodes
