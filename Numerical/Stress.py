5d9e531e6ae197a109145585e64906ae62935bfd
unknown <i.benyahia@hotmail.com>
2020-02-28 11:04:52 +0100

    stress updated

--- Numerical/Stress.py
+++ Numerical/Stress.py
@@ -13,12 +13,39 @@ import matplotlib.pyplot as plt
 import Deflection as d
 import Parameters as c
 import ShearDist as sh
-import J as tor
+import TorsionShear as tor
+from mpl_toolkits.mplot3d import Axes3D
 #%%
 
-def bending_stress(xloc,yloc,zloc):
-    return d.M_z(xloc)*yloc/sim.Izz + d.M_y(xloc)*zloc/sim.Iyy
+z_1 = np.linspace(-sim.z_t,-sim.z_t+c.h/2,5000)
+z_2 = np.linspace(-sim.z_t,-c.Ca+c.h/2-sim.z_t,5000)
+z_3 = np.linspace(-c.Ca+c.h/2-sim.z_t,-sim.z_t,5000)
+z_4 = -sim.z_t*np.ones(5000)
 
+def circle(z1):
+    return np.sqrt(np.abs((c.h/2)**2 - (z1+sim.z_t)**2))
+def circle_rev(z1):
+    return -np.sqrt(np.abs((c.h/2)**2 - (z1+sim.z_t)**2))
+def len_incl_1(z1):
+    return z1*np.tan(c.beta_rad) + sim.z_t*np.tan(c.beta_rad)+c.h/2
+def len_incl_2(z1):
+    return -(z1+sim.z_t)*np.tan(c.beta_rad) - c.h/2
+def spar_h(z1):
+    return np.linspace(0,-c.h/2,5000)
+def spar_h_rev(z1):
+    return np.linspace(0,c.h/2,5000)
+
+def bending_stress(xloc):
+    
+    
+    bending_circle = d.M_z(xloc)*circle(z_1)/sim.Izz + d.M_y(xloc)*z_1/sim.Iyy
+    bending_circle_rev = d.M_z(xloc)*circle_rev(z_1)/sim.Izz + d.M_y(xloc)*z_1/sim.Iyy
+    bending_incl_1 = d.M_z(xloc)*len_incl_1(z_2)/sim.Izz + d.M_y(xloc)*z_2/sim.Iyy
+    bending_incl_2 = d.M_z(xloc)*len_incl_2(z_3)/sim.Izz + d.M_y(xloc)*z_3/sim.Iyy
+    bending_spar = d.M_z(xloc)*spar_h(z_4)/sim.Izz + d.M_y(xloc)*z_4/sim.Iyy
+    bending_spar_rev = d.M_z(xloc)*spar_h_rev(z_4)/sim.Izz + d.M_y(xloc)*z_4/sim.Iyy
+    
+    return bending_circle,bending_circle_rev,bending_incl_1,bending_incl_2,bending_spar,bending_spar_rev
 
 
 def shear_stress(xloc):
@@ -44,10 +71,24 @@ def shear_stress(xloc):
     return tau_yz_circle,tau_yz_circle_rev,tau_yz_incl_1,tau_yz_incl_2,tau_yz_spar,tau_yz_spar_rev
 
 
-test = shear_stress(0.5)
+
+def Von_Mises(xloc):
     
-plt.plot(test[0])
+    vm_circle = np.sqrt(bending_stress(xloc)[0]**2 + 3*shear_stress(xloc)[0]**2)
+    vm_circle_rev = np.sqrt(bending_stress(xloc)[1]**2 + 3*shear_stress(xloc)[1]**2)
+    vm_incl_1 = np.sqrt(bending_stress(xloc)[2]**2 + 3*shear_stress(xloc)[2]**2)
+    vm_incl_2 = np.sqrt(bending_stress(xloc)[3]**2 + 3*shear_stress(xloc)[3]**2)
+    vm_spar = np.sqrt(bending_stress(xloc)[4]**2 + 3*shear_stress(xloc)[4]**2)
+    vm_spar_rev = np.sqrt(bending_stress(xloc)[5]**2 + 3*shear_stress(xloc)[5]**2)
     
+    return vm_circle,vm_circle_rev,vm_incl_1,vm_incl_2,vm_spar,vm_spar_rev
+
+#%%
     
-    
+plt.plot(z_1,circle(z_1))
+plt.plot(z_1,circle_rev(z_1))
+plt.plot(z_2,len_incl_1(z_2))
+plt.plot(z_3,len_incl_2(z_3))
+plt.plot(z_4,spar_h(z_4))
+plt.plot(z_4,spar_h_rev(z_4))
     
